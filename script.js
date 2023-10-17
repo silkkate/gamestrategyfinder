9c82a2e1bf208efe751b1e72bf49c8d1b7fba490
silkkate <silk.kate95@gmail.com>
2023-10-17 14:27:53 -0400

    fetched and formatted response

--- script.js
+++ script.js
@@ -1,31 +1,34 @@
 const gameTitleInput = document.getElementById("gameTitle");
 const strategyButton = document.getElementById("strategyButton");
-const apiKey = key;
-const openAiUrl = "https://api.openai.com/v1/engines/davinci/completions";
+const apiKey = `${process.env.WEATHER_API_KEY}`;
+const openAiUrl = "https://api.openai.com/v1/engines/text-davinci-003/completions";
 
-strategyButton.addEventListener('click', (event) => {
-    console.log("Button was clicked");
-    event.preventDefault(); 
-const gameTitle = gameTitleInput.value;
-const aiPrompt = `Give me the best strategy to beat ${gameTitle}.`;
-getGameStrat(aiPrompt);
-});
-
-function getGameStrat(x){
+function getGameStrategy() {
+    const gameTitle = gameTitleInput.value;
+    const aiPrompt = `Give me the best strategy to beat ${gameTitle}.`;
     fetch(openAiUrl, {
         method: "POST",
         headers: {
-            "Authorization":  `Bearer ${apiKey}`,
-            "Content-Type": "application/json",
+            'Authorization': `Bearer ${apiKey}`,
+            'Content-Type': 'application/json',
         },
         body: JSON.stringify({
-            prompt: x,
+            prompt: aiPrompt,
             max_tokens: 1000,
         }),
     })
     .then(response => response.json())
     .then(data => {
-        console.log("Full response data:", data);
         console.log(data.choices[0].text.trim());
+        let formattedText = data.choices[0].text.trim().replace(/\n/g, '<br>');
+        document.getElementById('gameStrategy').innerHTML = formattedText;
     })
 }
+
+    strategyButton.addEventListener('click', getGameStrategy);
+    gameTitleInput.addEventListener('keydown', (event) => {
+        if (event.keyCode === 13) { 
+            getGameStrategy(); 
+        }
+    });
+
