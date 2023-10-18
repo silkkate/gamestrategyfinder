a89ca472656296b3934f341dc5180a3bdca1ba6e
silkkate <silk.kate95@gmail.com>
2023-10-18 19:47:05 -0400

    edit nex.config

--- next.config.js
+++ next.config.js
@@ -1,6 +1,6 @@
 module.exports = {
   reactStrictMode: true,
     env: {
-      NEXT_PUBLIC_GAME_API_KEY: process.env.NEXT_PUBLIC_GAME_API_KEY,
+      NEXT_PUBLIC_GAME_API_KEY: process.env.GAME_API_KEY,
     },
   };
