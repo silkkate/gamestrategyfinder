dab37f52add9c4d70fe151c18d8991fa2686315c
silkkate <silk.kate95@gmail.com>
2023-10-18 13:57:55 -0400

    undo previous changes

--- script.js
+++ script.js
@@ -1,16 +1,20 @@
 const gameTitleInput = document.getElementById("gameTitle");
 const strategyButton = document.getElementById("strategyButton");
+const apiKey = process.env.NEXT_PUBLIC_GAME_API_KEY;
+const openAiUrl = "https://api.openai.com/v1/engines/text-davinci-003/completions";
 
 function getGameStrategy() {
     const gameTitle = gameTitleInput.value;
     const aiPrompt = `Give me the best strategy to beat ${gameTitle}.`;
-    fetch('/api/fetchData', {
+    fetch(openAiUrl, {
         method: "POST",
         headers: {
+            'Authorization': `Bearer ${apiKey}`,
             'Content-Type': 'application/json',
         },
         body: JSON.stringify({
-            gameTitle: gameTitle
+            prompt: aiPrompt,
+            max_tokens: 1000,
         }),
     })
     .then(response => response.json())
